d11611990c010e5d1a14cbbc0740411523841b24
ANAND KR. AGRAHARI <163381623+Shasvat-Anand@users.noreply.github.com>
2026-04-23 11:56:39 +0530

    Add files via upload
    add some styling

--- App.jsx
+++ App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
-import Header from './components/Header.jsx'
-import MovingItem from './components/MovingItem.jsx'
-import PopularDish from './components/PopularDish.jsx'
+import Header from './components/Header/Header.jsx'
+import MovingItem from './components/MovingItem/MovingItem.jsx'
+import PopularDish from './components/PopularDish/PopularDish.jsx'
 
-import Starter from './components/Starter.jsx'
+import Starter from './components/Starter/Starter.jsx'
+import Sweets from './components/Sweets/Sweets.jsx'
+
+import Aboutus from './components/Aboutus/Aboutus.jsx'
+import Footer from './components/Footer/Footer.jsx'
+ 
 
  
 import './App.css'
@@ -13,12 +18,19 @@ function App() {
 
   return (
     <>
+    <div className='web'>
     <Header />
     <MovingItem />
+    <Aboutus/>
 
     <PopularDish/>
     <Starter/>
+
+    <Sweets/>
     
+    <Footer/>
+    
+      </div>
      
       
     </>
